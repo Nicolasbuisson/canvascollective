a1552046e56d593ff62723a2c4be137a1021ac4f
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2024-12-22 16:43:57 -0500

    cicd to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/canvascollective',
+  reactStrictMode: true
 };
 
 export default nextConfig;
