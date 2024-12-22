f3146e133b7c366198fa6d2c9638230c024988ee
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2024-12-22 17:04:49 -0500

    fix cicd bug try

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/canvascollective',
+  images: {
+    unoptimized: true
+},
   reactStrictMode: true
 };
 
