834ceb6b8e4b3df17c77252d608d3f4a961ba39e
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2025-01-08 16:24:43 -0500

    schedule component

---
+++ src/components/schedule/schedule.tsx
@@ -0,0 +1,20 @@
+import "./schedule.css";
+
+export const Schedule = () => {
+  return (
+    <section className="schedule-section">
+      <hr />
+      <h2>Schedule</h2>
+      <div>
+        <div className="schedule-entry">
+          <p>TUESDAY - FRIDAY:</p>
+          <p>11AM - 7PM</p>
+        </div>
+        <div className="schedule-entry">
+          <p>SATURDAY:</p>
+          <p>11AM - 5PM</p>
+        </div>
+      </div>
+    </section>
+  );
+};
